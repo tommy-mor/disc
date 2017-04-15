7bba4a630fcf54577471ae479070df0770a29fee
tommy morriss <thmorriss@gmail.com>
2017-04-15 01:26:52 -0700

    added more basic gui

--- app.py
+++ app.py
@@ -1,11 +1,37 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QMainWindow, QAction, qApp
+from PyQt5.QtCore import QCoreApplication
+from PyQt5.QtGui import QIcon
+
+class LoginScreen(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.initScreen()
+    def initScreen(self):
+        act = QAction(QIcon('icon.png'), 'Exit', self)
+        act.setStatusTip('exit the app')
+        act.triggered.connect(qApp.quit)
+
+        menubar = self.menuBar()
+        fileMenu = menubar.addMenu('&File')
+        fileMenu.addAction(act)
+
+        self.toolbar = self.addToolBar('exit')
+        self.toolbar.addAction(act)
+
+        self.setGeometry(300, 300, 250, 150)
+        self.setWindowTitle('disc')
+        self.statusBar().showMessage('Ready')
+
+        #btn = QPushButton('click me pls', self)
+        #btn.resize(btn.sizeHint())
+        #btn.move(20, 100)
+
+        self.show()
+    def keyPressEvent(self, e):
+        print(e)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    w = QWidget()
-    w.resize(250, 150)
-    w.move(300, 300)
-    w.setWindowTitle('discord client')
-    w.show()
+    login = LoginScreen()
     sys.exit(app.exec_())
